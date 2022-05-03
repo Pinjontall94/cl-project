abd33f7726ed51c57fdcac01b7e20d811ec26097
Sammi Johnson <sambjohnson94@gmail.com>
2022-05-03 11:59:38 -0700

    Modified skel src main to export main func

--- skeleton/src/main.lisp
+++ skeleton/src/main.lisp
@@ -1,5 +1,24 @@
 (defpackage <% @var name %>
-  (:use :cl))
+  (:use :cl)
+  (:export :main)
 (in-package :<% @var name %>)
 
-;; blah blah blah.
+(defun help ()
+  (format t "~&Usage:
+
+  <% @var name %> [name]~&"))
+
+(defun %main (argv)
+  "Parse CLI args."
+  (when (member "-h" argv :test #'equal)
+    (help)
+    (uiop:quit))
+  (greet  (or (first argv)
+              "dear lisp user")))
+
+(defun main ()
+  "Entry point for the executable.
+  Reads command line arguments."
+  ;; uiop:command-line-arguments returns a list of arguments (sans the script name).
+  ;; We defer the work of parsing to %main because we call it also from the Roswell script.
+  (%main (uiop:command-line-arguments)))
